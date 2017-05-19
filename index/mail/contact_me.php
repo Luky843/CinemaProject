90d6e9ef1770452ae3ef08b572140fc92487762f
Luky843 <lukas.oros@ixsol.sk>
2017-05-19 10:55:06 +0200

    Added contact php script

--- index/mail/contact_me.php
+++ index/mail/contact_me.php
@@ -14,12 +14,12 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // create email body and send it	
-$to = 'emailme@myprogrammingblog.com'; // hi mate thanks for purchase guna theme, just replace your email with emailme@myprogrammingblog.com
-$email_subject = "Contact form submitted by:  $name";
+$to = 'luky.oros@gmail.com'; // hi mate thanks for purchase guna theme, just replace your email with emailme@myprogrammingblog.com
+$email_subject = "New message from:  $name";
 $email_body = "You have received a new message. \n\n".
-				  " Here are the details:\n \nName: $name \n ".
-				  "Email: $email_address\n Message \n $message";
-$headers = "From: contacts@myprogrammingblog.com\n";
+				  "Here are the details:\n \nName: $name \n".
+				  "Email: $email_address\nMessage: $message";
+$headers = "From: info@ltscinema.sk\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
